296ff0505f2ae825e51b108102229502cdb9fab5
DimiterM <dimiter_m@yahoo.com>
2015-04-25 03:12:23 +0300

    Todo API

--- routes/todos.js
+++ routes/todos.js
@@ -5,24 +5,23 @@ var Todo = require('../models/todo');
 var dbApi = require('../helpers/dbApi');
 
 
-router.get('/list/:userId', function(req, res, next) {
+router.get('/list', function(req, res, next) {
 	dbApi.openConnection(function(db){
-		Todo.find({ userId: req.params.userId, isResolved: false }, function(err, todos){
-			/*var str = [];
-			todos.forEach(function(todo) {
-				str.push();
-			});*/
+		console.log(req.body, req.query);
+		Todo.find({ userId: req.body.userId, isResolved: false }, function(err, todos){
 			//res.json(todos/*.map(function(todo){ return todo.showTodo(); })*/);
-			res.render('todo', { title: 'Express', todos: todos, userId: req.params.userId });
+			//res.render('todo', { title: 'Express', todos: todos, userId: req.params.userId });
+			res.json(todos);
 			db.close();
 		});
 	});
 });
 
 
-router.get('/insert', function(req, res, next) {
+/*router.get('/insert', function(req, res, next) {
 	res.render('insertTodo', { title: 'Express', userId: req.query.userId });
-});
+	res.json();
+});*/
 
 
 router.post('/resolve', function(req, res, next) {
@@ -31,21 +30,23 @@ router.post('/resolve', function(req, res, next) {
 			{ _id: req.body.todoId, userId: req.body.userId }, 
 			{ isResolved: true } , 
 			function(err, numAffected){
-				res.redirect('/todos/list/' + req.body.userId);
+				//res.redirect('/todos/list/' + req.body.userId);
+				res.json(numAffected);
 				db.close();
 		});
 	});
 });
 
 
-router.post('/insertNewTodo', function(req, res, next) {
+router.post('/insert'/*'/insertNewTodo'*/, function(req, res, next) {
 	var newTodo = new Todo({
 		rawText: req.body.rawText,
 		userId: req.body.userId
 	});
 	dbApi.openConnection(function(db){
 		newTodo.save(function(err, saved){
-			res.redirect('/todos/list/' + req.body.userId);
+			//res.redirect('/todos/list/' + req.body.userId);
+			res.json(saved);
 			db.close();
 		});
 	});
