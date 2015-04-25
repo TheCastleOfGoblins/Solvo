c0f7b98a81cf2b5ff2c552b0d0d74d9a06ffac1e
EmanuelaMollova <emanuela.mollova@gmail.com>
2015-04-25 23:29:07 +0300

    Add routes for adding locations

--- routes/locations.js
+++ routes/locations.js
@@ -4,28 +4,21 @@ var router = express.Router();
 var Location = require('../models/location');
 
 router.get('/', function(req, res, next) {
-  	dbApi.openConnection(function(db){
-  		Location.find({"_user":req.session.passport.user._id}, function(err,contacts){
-  			res.json(contacts);
-  			db.close();
-  		});
-	});
-	console.log( req.session );
+	res.render('insertLocation', { title: 'Express' });
 });
 
 router.post('/create', function(req, res, next) {
-  	dbApi.openConnection(function(db){
-		var params = {
-			'name':req.body.name,
-		    'location':req.body.location,
-		    '_user':req.session.passport.user._id
-		};
-		var newLocation = new Contact(params);
-        newLocation.save(function(err, saved){
-        	res.json(saved);
-	        db.close();
-	    });
+	var newLocation = new Location({
+		name: req.body.name,
+    location: [req.body.locx, req.body.locy]
 	});
+	dbApi.openConnection(function(db){
+      newLocation.save(function(err, saved){
+			res.json(saved);
+			db.close();
+		});
+	});
+
 });
 
 module.exports = router;
