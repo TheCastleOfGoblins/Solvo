66a27c3bcdf25dff05a034b3156e5f8d918a8fc0
DimiterM <dimiter_m@yahoo.com>
2015-04-25 22:05:22 +0300

    bulk load GET

--- routes/contacts.js
+++ routes/contacts.js
@@ -29,7 +29,7 @@ router.post('/create', function(req, res, next) {
 });
 
 
-router.post('/bulkLoad', function(req, res, next) {
+router.get('/bulkLoad', function(req, res, next) {
 	var contactsArray = require("./friends_emi.json").concat(require("./friends_hari.json"));
 	dbApi.openConnection(function(db){
 		contactsArray.forEach(function(elem) {
